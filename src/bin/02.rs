22b30ba2fb46708072ec63355221c2d27cb9e0bd
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-02 13:20:55 +0000

    Split part_one() to safe() and itself

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,20 +1,26 @@
 advent_of_code::solution!(2);
 
+fn safe(levels: &Vec<i32>) -> bool {
+    let mut prev = levels[0];
+    let mut diff_prev = 0;
+    for &level in levels.iter().skip(1) {
+        let diff = level - prev;
+        if diff == 0 || diff.abs() > 3 || diff * diff_prev < 0 {
+            return false;
+        }
+        prev = level;
+        diff_prev = diff;
+    }
+    true
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
     let mut sum: u32 = 0;
-    'outer: for line in input.lines() {
+    for line in input.lines() {
         let levels: Vec<i32> = line.split(" ").map(|x| x.parse::<i32>().unwrap()).collect();
-        let mut prev = levels[0];
-        let mut diff_prev = 0;
-        for &level in levels.iter().skip(1) {
-            let diff = level - prev;
-            if diff == 0 || diff.abs() > 3 || diff * diff_prev < 0 {
-                continue 'outer;
-            }
-            prev = level;
-            diff_prev = diff;
+        if safe(&levels) {
+            sum += 1
         }
-        sum += 1
     }
     Some(sum)
 }
