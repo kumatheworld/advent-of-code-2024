4c99c7deea5b9f2a5993e1b8c2d79cf602acfed0
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-02 17:33:45 +0000

    Use itertools in day 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,44 +1,42 @@
+use itertools::Itertools;
+
 advent_of_code::solution!(2);
 
 fn safe(levels: &Vec<i32>) -> bool {
-    let mut prev = levels[0];
-    let mut diff_prev = 0;
-    for &level in levels.iter().skip(1) {
-        let diff = level - prev;
-        if diff == 0 || diff.abs() > 3 || diff * diff_prev < 0 {
-            return false;
-        }
-        prev = level;
-        diff_prev = diff;
-    }
-    true
+    let diff = levels
+        .iter()
+        .tuple_windows()
+        .map(|(m, n)| m - n)
+        .collect_vec();
+    let inc = diff.iter().all(|d| 1 <= *d && *d <= 3);
+    let dec = diff.iter().all(|d| -3 <= *d && *d <= -1);
+    inc || dec
+}
+
+fn parse(input: &str) -> impl Iterator<Item = Vec<i32>> + '_ {
+    input.lines().map(|line| {
+        line.split(" ")
+            .map(|elem| elem.parse::<i32>().unwrap())
+            .collect()
+    })
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let mut sum: u32 = 0;
-    for line in input.lines() {
-        let levels: Vec<i32> = line.split(" ").map(|x| x.parse::<i32>().unwrap()).collect();
-        if safe(&levels) {
-            sum += 1
-        }
-    }
-    Some(sum)
+    Some(parse(input).filter(safe).count() as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let mut sum: u32 = 0;
-    for line in input.lines() {
-        let levels: Vec<i32> = line.split(" ").map(|x| x.parse::<i32>().unwrap()).collect();
-        for i in 0..levels.len() {
-            let mut levels_removed = levels.clone();
-            levels_removed.remove(i);
-            if safe(&levels_removed) {
-                sum += 1;
-                break;
-            }
-        }
-    }
-    Some(sum)
+    Some(
+        parse(input)
+            .filter(|levels| {
+                (0..levels.len()).any(|i| {
+                    let mut levels_removed = levels.clone();
+                    levels_removed.remove(i);
+                    safe(&levels_removed)
+                })
+            })
+            .count() as u32,
+    )
 }
 
 #[cfg(test)]
