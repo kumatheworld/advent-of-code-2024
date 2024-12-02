fec06bb8f658af5672c201c11ccadb5e139fbe54
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-02 09:04:02 +0000

    Day 1 part 1 done

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,39 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut first_column = Vec::new();
+    let mut second_column = Vec::new();
+
+    for line in input.lines() {
+        let mut parts = line.split_whitespace();
+        let first = parts
+            .next()
+            .ok_or("Missing first column")
+            .ok()?
+            .trim()
+            .parse::<i32>()
+            .ok()?;
+        let second = parts
+            .next()
+            .ok_or("Missing second column")
+            .ok()?
+            .trim()
+            .parse::<i32>()
+            .ok()?;
+
+        first_column.push(first);
+        second_column.push(second);
+    }
+
+    first_column.sort();
+    second_column.sort();
+
+    let mut sum: u32 = 0;
+    for (first, second) in first_column.iter().zip(second_column.iter()) {
+        sum += (first - second).abs() as u32;
+    }
+
+    Some(sum)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
