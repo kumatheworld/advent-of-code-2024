d3a5ab66264c9d4c4df8b7cefa8f4e1f8d8c75c6
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-08 17:00:43 +0000

    Day 6 part 1 done

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,7 +1,36 @@
+use advent_of_code::Matrix;
+
 advent_of_code::solution!(6);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut mat = Matrix::from(input);
+    let (mut i, mut j) = mat.find(b'^').unwrap();
+
+    let di = [-1, 0, 1, 0];
+    let dj = [0, 1, 0, -1];
+    let mut d = 0;
+
+    mat[(i, j)] = b'X';
+    let mut sum: u32 = 1;
+    loop {
+        i += di[d];
+        j += dj[d];
+        match mat.get(i, j) {
+            Some(b'.') => {
+                sum += 1;
+                mat[(i, j)] = b'X';
+            }
+            Some(b'#') => {
+                i -= di[d];
+                j -= dj[d];
+                d = (d + 1) % 4;
+            }
+            Some(b'X') => continue,
+            Some(_) => panic!(),
+            None => break,
+        }
+    }
+    Some(sum)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
