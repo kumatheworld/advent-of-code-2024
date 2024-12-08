265dd04bbe36e32a93f8db36896e46e57f9a5875
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-08 14:48:20 +0000

    Add Matrix

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
 pub mod template;
 
 // Use this file to add helper functions and additional modules.
+#[derive(Debug)]
+pub struct Matrix<'a> {
+    matrix: &'a [u8],
+    pub rows: usize,
+    pub cols: usize,
+}
