8fbcadb93f7127942ae1d1c7230298fedc420abb
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-09 22:05:12 +0000

    Day 8 part 2 done

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,7 +1,6 @@
 use advent_of_code::Matrix;
-use itertools::iproduct;
+use itertools::{iproduct, iterate};
 use std::collections::HashMap;
-
 advent_of_code::solution!(8);
 
 fn prepare(input: &str) -> (Matrix, HashMap<u8, Vec<(i32, i32)>>) {
@@ -46,7 +45,24 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (mut mat, antennas) = prepare(input);
+    let m = mat.rows as i32;
+    let n = mat.cols as i32;
+    Some(
+        antennas
+            .iter()
+            .map(|(_, v)| {
+                iproduct!(v, v)
+                    .filter(|(p, q)| p != q)
+                    .flat_map(|(&(i0, j0), &(i1, j1))| {
+                        iterate((i1, j1), move |&(i, j)| (i + i1 - i0, j + j1 - j0))
+                            .take_while(|&(i, j)| (0..m).contains(&i) && (0..n).contains(&j))
+                    })
+                    .filter(|&(i, j)| build_antenna(&mut mat, i, j))
+                    .count() as u32
+            })
+            .sum(),
+    )
 }
 
 #[cfg(test)]
