2f753d584fa44a4fa81b1439ea3e17281397763c
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-11 18:17:54 +0000

    Unify part 1 and part 2 using common()

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -4,9 +4,11 @@ use std::collections::HashMap;
 
 advent_of_code::solution!(8);
 
-fn prepare(input: &str) -> (Matrix, HashMap<u8, Vec<(i32, i32)>>) {
+fn common<F>(input: &str, yield_antennas: F) -> Option<u32>
+where
+    F: Fn((i32, i32), (i32, i32), &Matrix) -> Box<dyn Iterator<Item = (i32, i32)> + '_>,
+{
     let mat = Matrix::from(input);
-
     let mut antennas: HashMap<u8, Vec<(i32, i32)>> = HashMap::new();
     for i in 0..mat.rows as i32 {
         for j in 0..mat.cols as i32 {
@@ -19,44 +21,34 @@ fn prepare(input: &str) -> (Matrix, HashMap<u8, Vec<(i32, i32)>>) {
         }
     }
 
-    (mat, antennas)
+    Some(
+        antennas
+            .iter()
+            .flat_map(|(_, v)| {
+                iproduct!(v, v)
+                    .filter(|(p, q)| p != q)
+                    .flat_map(|(&(i0, j0), &(i1, j1))| yield_antennas((i0, j0), (i1, j1), &mat))
+            })
+            .unique()
+            .count() as u32,
+    )
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let (mat, antennas) = prepare(input);
-    Some(
-        antennas
-            .iter()
-            .flat_map(|(_, v)| {
-                iproduct!(v, v)
-                    .filter(|(p, q)| p != q)
-                    .filter_map(|((i0, j0), (i1, j1))| {
-                        let i2 = 2 * i1 - i0;
-                        let j2 = 2 * j1 - j0;
-                        mat.get(i2, j2).is_some().then(|| (i2, j2))
-                    })
-            })
-            .unique()
-            .count() as u32,
-    )
+    common(input, |(i0, j0), (i1, j1), mat| {
+        let i2 = 2 * i1 - i0;
+        let j2 = 2 * j1 - j0;
+        Box::new(mat.get(i2, j2).is_some().then(|| (i2, j2)).into_iter())
+    })
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let (mat, antennas) = prepare(input);
-    Some(
-        antennas
-            .iter()
-            .flat_map(|(_, v)| {
-                iproduct!(v, v)
-                    .filter(|(p, q)| p != q)
-                    .flat_map(|(&(i0, j0), &(i1, j1))| {
-                        iterate((i1, j1), move |&(i, j)| (i + i1 - i0, j + j1 - j0))
-                            .take_while(|&(i, j)| mat.get(i, j).is_some())
-                    })
-            })
-            .unique()
-            .count() as u32,
-    )
+    common(input, |(i0, j0), (i1, j1), mat| {
+        Box::new(
+            iterate((i1, j1), move |(i, j)| (i + i1 - i0, j + j1 - j0))
+                .take_while(|&(i, j)| mat.get(i, j).is_some()),
+        )
+    })
 }
 
 #[cfg(test)]
