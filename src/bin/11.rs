237074ea2cb619ab84dbb728b6fe9bbb4e8de3f7
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-14 23:28:24 +0000

    Use u64 for day 11 part 2

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -21,7 +21,7 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(buf.count() as u64)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<u64> {
     None
 }
 
