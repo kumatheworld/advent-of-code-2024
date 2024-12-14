388bf13159e7d2eeebeadb99dd436121e6042c6d
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-14 23:23:05 +0000

    Day 11 part 1 done

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,7 +1,24 @@
 advent_of_code::solution!(11);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<u64> {
+    let mut buf: Box<dyn Iterator<Item = String>> =
+        Box::new(input.trim().split(' ').map(|s| s.to_string()));
+
+    for _ in 0..25 {
+        buf = Box::new(buf.flat_map(|mut s| {
+            s.drain(..s.find(|c| c != '0').unwrap_or(s.len()));
+            if s.is_empty() {
+                vec!["1".to_string()]
+            } else if s.len() & 1 == 0 {
+                let latter = s.split_off(s.len() >> 1);
+                vec![s, latter]
+            } else {
+                vec![(s.parse::<u64>().unwrap() * 2024).to_string()]
+            }
+        }))
+    }
+
+    Some(buf.count() as u64)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
