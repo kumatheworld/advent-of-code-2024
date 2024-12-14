526e866a7cc2ded16740409ca6c0212a2c3db2a2
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-14 22:09:23 +0000

    Day 10 part 1 done

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,7 +1,31 @@
+use advent_of_code::Matrix;
+use itertools::{iproduct, Itertools};
+
 advent_of_code::solution!(10);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    static D: [(i32, i32); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+    let mat = Matrix::from(input);
+    let zeros =
+        iproduct!(0..mat.rows as i32, 0..mat.cols as i32).filter(|&(i, j)| mat[(i, j)] == b'0');
+
+    Some(
+        zeros
+            .map(|(i0, j0)| {
+                let mut buf: Box<dyn Iterator<Item = (i32, i32)>> =
+                    Box::new(std::iter::once((i0, j0)));
+                for level in b'1'..=b'9' {
+                    let mat_ref = &mat;
+                    buf = Box::new(
+                        buf.flat_map(|(i, j)| D.map(|(di, dj)| (i + di, j + dj)))
+                            .filter(move |&(i, j)| mat_ref.get(i, j) == Some(level))
+                            .unique(),
+                    );
+                }
+                buf.count() as u32
+            })
+            .sum(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
