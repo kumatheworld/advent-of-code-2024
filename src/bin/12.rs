38eb85104b33e21a176574c7dcb21a1cd7f43390
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-18 21:57:01 +0000

    Day 12 part 1 done

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,7 +1,43 @@
+use advent_of_code::Matrix;
+
 advent_of_code::solution!(12);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    const DIJ: [(i32, i32); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+    let mat = Matrix::from(input);
+    let mut seen = mat.new_uniform(false);
+
+    let mut sum = 0;
+    for (i, j) in mat.indices() {
+        if seen[(i, j)] {
+            continue;
+        }
+
+        let c = mat[(i, j)];
+        seen[(i, j)] = true;
+        let mut stack = vec![(i, j)];
+        let mut area = 1;
+        let mut perimeter = 4;
+        while !stack.is_empty() {
+            let (ii, jj) = stack.pop().unwrap();
+            let indices = DIJ.iter().filter_map(|&(di, dj)| {
+                (mat.get(ii + di, jj + dj) == Some(c)).then_some((ii + di, jj + dj))
+            });
+            for (iii, jjj) in indices {
+                if seen[(iii, jjj)] {
+                    perimeter -= 1;
+                } else {
+                    seen[(iii, jjj)] = true;
+                    area += 1;
+                    perimeter += 3;
+                    stack.push((iii, jjj));
+                }
+            }
+        }
+        sum += area * perimeter
+    }
+
+    Some(sum)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
