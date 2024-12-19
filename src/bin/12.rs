62f1160d7afe5c3e0b72124c4e96a34210af94b0
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-19 18:41:15 +0000

    Unify day 12 part 1 and 2

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -3,8 +3,9 @@ use itertools::Itertools;
 
 advent_of_code::solution!(12);
 
-pub fn part_one(input: &str) -> Option<u32> {
+fn common(input: &str, use_perimeter: bool) -> Option<u32> {
     const DIJ: [(i32, i32); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+
     let mat = Matrix::from(input);
     let mut seen = mat.new_uniform(false);
 
@@ -14,95 +15,75 @@ pub fn part_one(input: &str) -> Option<u32> {
             continue;
         }
 
-        let c = mat[(i, j)];
+        let mut connected = mat.new_uniform(false);
+        connected[(i, j)] = true;
         seen[(i, j)] = true;
+
         let mut stack = vec![(i, j)];
         let mut area = 1;
         let mut perimeter = 4;
+
         while !stack.is_empty() {
             let (ii, jj) = stack.pop().unwrap();
             let indices = DIJ.iter().filter_map(|&(di, dj)| {
-                (mat.get(ii + di, jj + dj) == Some(c)).then_some((ii + di, jj + dj))
+                (mat.get(ii + di, jj + dj) == Some(mat[(i, j)])).then_some((ii + di, jj + dj))
             });
             for (iii, jjj) in indices {
                 if seen[(iii, jjj)] {
                     perimeter -= 1;
                 } else {
                     seen[(iii, jjj)] = true;
+                    connected[(iii, jjj)] = true;
                     area += 1;
                     perimeter += 3;
                     stack.push((iii, jjj));
                 }
             }
         }
-        sum += area * perimeter
+
+        sum += area
+            * if use_perimeter {
+                perimeter
+            } else {
+                let connected = &connected;
+                (0..=mat.rows as i32)
+                    .flat_map(|i| {
+                        (0..mat.cols as i32)
+                            .map(move |j| {
+                                connected
+                                    .get(i - 1, j)
+                                    .or(Some(false))
+                                    .cmp(&connected.get(i, j).or(Some(false)))
+                            })
+                            .dedup()
+                    })
+                    .filter(|&o| o.is_ne())
+                    .count() as u32
+                    + (0..=mat.cols as i32)
+                        .flat_map(|j| {
+                            (0..mat.rows as i32)
+                                .map(move |i| {
+                                    connected
+                                        .get(i, j - 1)
+                                        .or(Some(false))
+                                        .cmp(&connected.get(i, j).or(Some(false)))
+                                })
+                                .dedup()
+                        })
+                        .filter(|&o| o.is_ne())
+                        .count() as u32
+            }
     }
 
     Some(sum)
 }
 
+pub fn part_one(input: &str) -> Option<u32> {
+    common(input, true)
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    const DIJ: [(i32, i32); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
-    let mat = Matrix::from(input);
-    let mut seen = mat.new_uniform(false);
-
-    let mut sum = 0;
-    for (i, j) in mat.indices() {
-        if seen[(i, j)] {
-            continue;
-        }
-
-        let c = mat[(i, j)];
-        seen[(i, j)] = true;
-        let mut stack = vec![(i, j)];
-        let mut area = 1;
-        let mut cs = seen.new_uniform(false);
-        cs[(i, j)] = true;
-        while !stack.is_empty() {
-            let (ii, jj) = stack.pop().unwrap();
-            let indices = DIJ.iter().filter_map(|&(di, dj)| {
-                (mat.get(ii + di, jj + dj) == Some(c)).then_some((ii + di, jj + dj))
-            });
-            for (iii, jjj) in indices {
-                if !seen[(iii, jjj)] {
-                    seen[(iii, jjj)] = true;
-                    cs[(iii, jjj)] = true;
-                    area += 1;
-                    stack.push((iii, jjj));
-                }
-            }
-        }
-
-        let cs = &cs;
-        let sides = (0..=mat.rows as i32)
-            .flat_map(|i| {
-                (0..mat.cols as i32)
-                    .map(move |j| {
-                        cs.get(i - 1, j)
-                            .or(Some(false))
-                            .cmp(&cs.get(i, j).or(Some(false)))
-                    })
-                    .dedup()
-            })
-            .filter(|&o| o.is_ne())
-            .count() as u32
-            + (0..=mat.cols as i32)
-                .flat_map(|j| {
-                    (0..mat.rows as i32)
-                        .map(move |i| {
-                            cs.get(i, j - 1)
-                                .or(Some(false))
-                                .cmp(&cs.get(i, j).or(Some(false)))
-                        })
-                        .dedup()
-                })
-                .filter(|&o| o.is_ne())
-                .count() as u32;
-
-        sum += area * sides
-    }
-
-    Some(sum)
+    common(input, false)
 }
 
 #[cfg(test)]
