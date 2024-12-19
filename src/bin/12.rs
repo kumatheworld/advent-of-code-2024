0fb3c5dbc6cb62ff51ce145de82944fe75487a36
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-19 16:03:09 +0000

    Day 12 part 2 done

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,4 +1,5 @@
 use advent_of_code::Matrix;
+use itertools::Itertools;
 
 advent_of_code::solution!(12);
 
@@ -41,7 +42,67 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    const DIJ: [(i32, i32); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+    let mat = Matrix::from(input);
+    let mut seen = mat.new_uniform(false);
+
+    let mut sum = 0;
+    for (i, j) in mat.indices() {
+        if seen[(i, j)] {
+            continue;
+        }
+
+        let c = mat[(i, j)];
+        seen[(i, j)] = true;
+        let mut stack = vec![(i, j)];
+        let mut area = 1;
+        let mut cs = seen.new_uniform(false);
+        cs[(i, j)] = true;
+        while !stack.is_empty() {
+            let (ii, jj) = stack.pop().unwrap();
+            let indices = DIJ.iter().filter_map(|&(di, dj)| {
+                (mat.get(ii + di, jj + dj) == Some(c)).then_some((ii + di, jj + dj))
+            });
+            for (iii, jjj) in indices {
+                if !seen[(iii, jjj)] {
+                    seen[(iii, jjj)] = true;
+                    cs[(iii, jjj)] = true;
+                    area += 1;
+                    stack.push((iii, jjj));
+                }
+            }
+        }
+
+        let cs = &cs;
+        let sides = (0..=mat.rows as i32)
+            .flat_map(|i| {
+                (0..mat.cols as i32)
+                    .map(move |j| {
+                        cs.get(i - 1, j)
+                            .or(Some(false))
+                            .cmp(&cs.get(i, j).or(Some(false)))
+                    })
+                    .dedup()
+            })
+            .filter(|&o| o.is_ne())
+            .count() as u32
+            + (0..=mat.cols as i32)
+                .flat_map(|j| {
+                    (0..mat.rows as i32)
+                        .map(move |i| {
+                            cs.get(i, j - 1)
+                                .or(Some(false))
+                                .cmp(&cs.get(i, j).or(Some(false)))
+                        })
+                        .dedup()
+                })
+                .filter(|&o| o.is_ne())
+                .count() as u32;
+
+        sum += area * sides
+    }
+
+    Some(sum)
 }
 
 #[cfg(test)]
