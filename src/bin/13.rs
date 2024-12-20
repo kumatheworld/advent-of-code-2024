79fd593895ed1677a625d28014c7d578ca833a75
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-20 19:37:00 +0000

    Day 13 part 2 done

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,13 +1,11 @@
-use std::ops::RangeInclusive;
+use std::ops::Range;
 
 use itertools::Itertools;
 use regex::Regex;
 
 advent_of_code::solution!(13);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    const LE100: RangeInclusive<i32> = 0..=100;
-
+fn common(input: &str, range: Range<i64>, offset: i64) -> Option<u64> {
     let re = Regex::new(
         r"^Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)$",
     )
@@ -18,14 +16,17 @@ pub fn part_one(input: &str) -> Option<u32> {
             .trim()
             .split("\n\n")
             .filter_map(|machine| {
-                let (ax, ay, bx, by, px, py) = re
+                let (ax, ay, bx, by, px_, py_) = re
                     .captures(machine)
                     .unwrap()
                     .iter()
                     .skip(1)
-                    .map(|x| x.unwrap().as_str().parse::<i32>().unwrap())
+                    .map(|x| x.unwrap().as_str().parse::<i64>().unwrap())
                     .collect_tuple()
                     .unwrap();
+
+                let px = px_ + offset;
+                let py = py_ + offset;
 
                 let det = ax * by - bx * ay;
                 if det == 0 {
@@ -35,16 +36,20 @@ pub fn part_one(input: &str) -> Option<u32> {
                     let bdet = ax * py - ay * px;
                     let a = adet / det;
                     let b = bdet / det;
-                    (LE100.contains(&a) && LE100.contains(&b) && adet % det == 0 && bdet % det == 0)
-                        .then(|| 3 * a as u32 + b as u32)
+                    (range.contains(&a) && range.contains(&b) && adet % det == 0 && bdet % det == 0)
+                        .then(|| 3 * a as u64 + b as u64)
                 }
             })
             .sum(),
     )
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<u64> {
+    common(input, 0..100, 0)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    common(input, 0..i64::MAX, 10000000000000)
 }
 
 #[cfg(test)]
@@ -60,6 +65,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(875318608908)); // Test written after the submission was accepted
     }
 }
