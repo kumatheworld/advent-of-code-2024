95cec6eca1ee2c51c630a7305694f89f0f20305b
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-21 13:20:04 +0000

    Split day 14 part_one() into common() and itself

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -3,29 +3,36 @@ use regex::Regex;
 
 advent_of_code::solution!(14);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    const SECONDS: i32 = 100;
+fn common(input: &str) -> ((i32, i32), impl Iterator<Item = (i32, i32, i32, i32)> + '_) {
     const BOUNDARY: usize = 1000;
     let re = Regex::new(r"p=(\d+),(\d+) v=(-?\d+),(-?\d+)$").unwrap();
 
-    let (rows, cols) = if input.len() < BOUNDARY {
+    let size = if input.len() < BOUNDARY {
         (7, 11)
     } else {
         (103, 101)
     };
 
+    let ppvvs = input.lines().map(move |line| {
+        re.captures(line)
+            .unwrap()
+            .iter()
+            .skip(1)
+            .map(|x| x.unwrap().as_str().parse::<i32>().unwrap())
+            .collect_tuple()
+            .unwrap()
+    });
+
+    (size, ppvvs)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    const SECONDS: i32 = 100;
+    let ((rows, cols), ppvvs) = common(input);
+
     Some(
-        input
-            .lines()
-            .filter_map(|line| {
-                let (px, py, vx, vy) = re
-                    .captures(line)
-                    .unwrap()
-                    .iter()
-                    .skip(1)
-                    .map(|x| x.unwrap().as_str().parse::<i32>().unwrap())
-                    .collect_tuple()
-                    .unwrap();
+        ppvvs
+            .filter_map(|(px, py, vx, vy)| {
                 let x = (px + SECONDS * vx).rem_euclid(cols).cmp(&(cols >> 1));
                 let y = (py + SECONDS * vy).rem_euclid(rows).cmp(&(rows >> 1));
                 (![x, y].contains(&std::cmp::Ordering::Equal)).then_some((x, y))
