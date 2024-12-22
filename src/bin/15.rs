c6f9d6ec24c6c20bc32f88e34037ad0fc5e87831
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-22 11:07:00 +0000

    Day 15 part 1 done

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,7 +1,49 @@
+use core::panic;
+
+use advent_of_code::Matrix;
+
 advent_of_code::solution!(15);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    const MULTIPLIER: i32 = 100;
+
+    let (warehouse, directions) = input.split_once("\n\n").unwrap();
+
+    let mut mat = Matrix::from(warehouse);
+    let (mut i, mut j) = mat.position(b'@').unwrap();
+    mat[(i, j)] = b'.';
+
+    let directions = directions.chars().filter_map(|c| match c {
+        '^' => Some((-1, 0)),
+        '>' => Some((0, 1)),
+        'v' => Some((1, 0)),
+        '<' => Some((0, -1)),
+        '\n' => None,
+        _ => panic!(),
+    });
+
+    'outer: for (di, dj) in directions {
+        let mut k = 1;
+        loop {
+            match mat.get(i + k * di, j + k * dj) {
+                Some(b'.') => break,
+                Some(b'O') => (),
+                Some(b'#') => continue 'outer,
+                _ => panic!(),
+            }
+            k += 1;
+        }
+
+        mat.swap((i + di, j + dj), (i + k * di, j + k * dj));
+        i = i + di;
+        j = j + dj;
+    }
+
+    Some(
+        mat.indices()
+            .filter_map(|(i, j)| (mat[(i, j)] == b'O').then(|| (MULTIPLIER * i + j) as u32))
+            .sum(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
