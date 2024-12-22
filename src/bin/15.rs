5e963290b09099fa019d9557b0209087c96b4fdd
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-22 22:46:49 +0000

    Fix day 15 part 2 test

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -94,6 +94,6 @@ mod tests {
         let result = part_two(&advent_of_code::template::read_file_part(
             "examples", DAY, 2,
         ));
-        assert_eq!(result, Some(9201));
+        assert_eq!(result, Some(9021));
     }
 }
