4c75d4ba8e68b8372c7d0b740eee1a8841301f34
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-27 19:32:38 +0000

    Write tests for day 16 part 2

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -83,6 +83,14 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(45));
+    }
+
+    #[test]
+    fn test_part_two_2() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(64));
     }
 }
