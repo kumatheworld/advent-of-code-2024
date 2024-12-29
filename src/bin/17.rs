d5831cec428b98f366b2ab45e6821826d9cd7517
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-29 16:56:32 +0000

    Add day 17 part 1 tests

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -18,6 +18,22 @@ mod tests {
     }
 
     #[test]
+    fn test_part_one_2() {
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some("0,1,2".to_string()));
+    }
+
+    #[test]
+    fn test_part_one_3() {
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 3,
+        ));
+        assert_eq!(result, Some("4,2,5,6,7,7,7,7,3,1,0".to_string()));
+    }
+
+    #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, None);
