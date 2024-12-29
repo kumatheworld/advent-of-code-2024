0805ecc239c204af249366b7a67678d52343f58b
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-29 16:56:44 +0000

    Day 17 part 1 done

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,6 +1,61 @@
+use itertools::Itertools;
+
 advent_of_code::solution!(17);
 
 pub fn part_one(input: &str) -> Option<String> {
+    let re = regex::Regex::new(
+        r"^Register A: (\d+)\nRegister B: (\d+)\nRegister C: (\d+)\n\nProgram: (.*)$",
+    )
+    .unwrap();
+    let (a, b, c, program) = re
+        .captures(input.trim())
+        .unwrap()
+        .iter()
+        .skip(1)
+        .map(|x| x.unwrap().as_str())
+        .collect_tuple()
+        .unwrap();
+    let mut a = a.parse::<u32>().unwrap();
+    let mut b = b.parse::<u32>().unwrap();
+    let mut c = c.parse::<u32>().unwrap();
+
+    let mut output = vec![];
+    let program = program
+        .split(',')
+        .map(|n| n.parse::<u8>().unwrap())
+        .tuples()
+        .collect_vec();
+
+    let mut pointer = 0;
+    while pointer < program.len() {
+        let (opcode, literal) = program[pointer];
+        let combo = match literal {
+            n if n < 4 => n as u32,
+            4 => a,
+            5 => b,
+            6 => c,
+            _ => unreachable!(),
+        };
+        match opcode {
+            0 => a >>= combo,
+            1 => b ^= literal as u32,
+            2 => b = combo % 8,
+            3 => {
+                if a != 0 {
+                    pointer = (literal >> 1) as usize;
+                    continue;
+                }
+            }
+            4 => b ^= c,
+            5 => output.push((combo % 8) as u8),
+            6 => b = a >> combo,
+            7 => c = a >> combo,
+            _ => unreachable!(),
+        };
+        pointer += 1;
+    }
+
+    Some(output.into_iter().map(|n| n.to_string()).join(","))
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
