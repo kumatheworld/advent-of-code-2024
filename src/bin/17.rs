b502f484fd9db4f924a8b7c966b674aeb1b44706
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-29 16:56:08 +0000

    Fix day 17 part 1 test

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,7 +1,6 @@
 advent_of_code::solution!(17);
 
-pub fn part_one(input: &str) -> Option<u64> {
-    None
+pub fn part_one(input: &str) -> Option<String> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
@@ -15,7 +14,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(4635635210));
+        assert_eq!(result, Some("4,6,3,5,6,3,5,2,1,0".to_string()));
     }
 
     #[test]
