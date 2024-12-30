d9a728a2a73625b17c319913489cdbc642b76710
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-30 14:14:33 +0000

    Move more into common()

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -3,7 +3,7 @@ use std::collections::{HashMap, HashSet};
 
 advent_of_code::solution!(5);
 
-fn common(input: &str) -> impl Iterator<Item = Result<u32, u32>> + '_ {
+fn common(input: &str, ok_or_err: fn(Result<u32, u32>) -> Option<u32>) -> Option<u32> {
     let (rules, updates) = input.split_once("\n\n").unwrap();
 
     let mut one2many: HashMap<u32, HashSet<u32>> = HashMap::new();
@@ -18,46 +18,52 @@ fn common(input: &str) -> impl Iterator<Item = Result<u32, u32>> + '_ {
             one2many.entry(m).or_insert_with(HashSet::new).insert(n);
         });
 
-    updates.lines().map(move |line| {
-        let pages = line
-            .split(',')
-            .map(|elem| elem.parse::<u32>().unwrap())
-            .collect_vec();
+    Some(
+        updates
+            .lines()
+            .map(move |line| {
+                let pages = line
+                    .split(',')
+                    .map(|elem| elem.parse::<u32>().unwrap())
+                    .collect_vec();
 
-        // Count the number of subsequent pages for every page
-        // It is a permutation of 0..pages.len() thanks to the fact that p|p does not hold
-        // and that either one of p|q or q|p holds for each distinct pair (p, q) of pages
-        let num_next_pages = pages
-            .iter()
-            .map(|p| {
-                pages
+                // Count the number of subsequent pages for every page
+                // It is a permutation of 0..pages.len() thanks to the fact that p|p does not hold
+                // and that either one of p|q or q|p holds for each distinct pair (p, q) of pages
+                let num_next_pages = pages
                     .iter()
-                    .copied()
-                    .collect::<HashSet<_>>()
-                    .intersection(one2many.get(p).unwrap_or(&HashSet::new()))
-                    .count()
+                    .map(|p| {
+                        pages
+                            .iter()
+                            .copied()
+                            .collect::<HashSet<_>>()
+                            .intersection(one2many.get(p).unwrap_or(&HashSet::new()))
+                            .count()
+                    })
+                    .collect_vec();
+
+                let mid = pages[num_next_pages
+                    .iter()
+                    .position(|&i| i == pages.len() >> 1)
+                    .unwrap()];
+
+                if num_next_pages == (0..pages.len()).rev().collect_vec() {
+                    Ok(mid)
+                } else {
+                    Err(mid)
+                }
             })
-            .collect_vec();
-
-        let mid = pages[num_next_pages
-            .iter()
-            .position(|&i| i == pages.len() >> 1)
-            .unwrap()];
-
-        if num_next_pages == (0..pages.len()).rev().collect_vec() {
-            Ok(mid)
-        } else {
-            Err(mid)
-        }
-    })
+            .filter_map(ok_or_err)
+            .sum(),
+    )
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(common(input).filter_map(|r| r.ok()).sum())
+    common(input, |r| r.ok())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    Some(common(input).filter_map(|r| r.err()).sum())
+    common(input, |r| r.err())
 }
 
 #[cfg(test)]
