6a00af85484e9b068b4659e6a030a1086b8b732d
kumatheworld <kum4th3w0rld@gmail.com>
2024-12-31 18:37:30 +0000

    Split day 17 part_one() into common() and itself

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -2,7 +2,12 @@ use itertools::Itertools;
 
 advent_of_code::solution!(17);
 
-pub fn part_one(input: &str) -> Option<String> {
+fn common(
+    input: &str,
+) -> (
+    Box<dyn FnMut(Vec<(u8, u8)>, Option<u32>) -> Box<dyn Iterator<Item = u8>>>,
+    Vec<(u8, u8)>,
+) {
     let re = regex::Regex::new(
         r"^Register A: (\d+)\nRegister B: (\d+)\nRegister C: (\d+)\n\nProgram: (.*)$",
     )
@@ -16,8 +21,47 @@ pub fn part_one(input: &str) -> Option<String> {
         .collect_tuple()
         .unwrap();
     let mut a = a.parse::<u32>().unwrap();
-    let mut b = b.parse::<u32>().unwrap();
-    let mut c = c.parse::<u32>().unwrap();
+    let b0 = b.parse::<u32>().unwrap();
+    let c0 = c.parse::<u32>().unwrap();
+
+    let executor = Box::new(move |program: Vec<(u8, u8)>, a0: Option<u32>| {
+        if let Some(a0) = a0 {
+            a = a0;
+        }
+        let mut b = b0;
+        let mut c = c0;
+        let mut pointer = 0;
+        Box::new(std::iter::from_fn(move || {
+            while pointer < program.len() {
+                let (opcode, literal) = program[pointer];
+                pointer += 1;
+                let combo = match literal {
+                    n if n < 4 => n as u32,
+                    4 => a,
+                    5 => b,
+                    6 => c,
+                    _ => unreachable!(),
+                };
+                match opcode {
+                    0 => a >>= combo,
+                    1 => b ^= literal as u32,
+                    2 => b = combo % 8,
+                    3 => {
+                        if a != 0 {
+                            pointer = (literal >> 1) as usize;
+                            continue;
+                        }
+                    }
+                    4 => b ^= c,
+                    5 => return Some((combo % 8) as u8),
+                    6 => b = a >> combo,
+                    7 => c = a >> combo,
+                    _ => unreachable!(),
+                };
+            }
+            None
+        })) as Box<dyn Iterator<Item = u8>>
+    });
 
     let program = program
         .split(',')
@@ -25,39 +69,12 @@ pub fn part_one(input: &str) -> Option<String> {
         .tuples()
         .collect_vec();
 
-    let mut pointer = 0;
-    let executor = std::iter::from_fn(|| {
-        while pointer < program.len() {
-            let (opcode, literal) = program[pointer];
-            pointer += 1;
-            let combo = match literal {
-                n if n < 4 => n as u32,
-                4 => a,
-                5 => b,
-                6 => c,
-                _ => unreachable!(),
-            };
-            match opcode {
-                0 => a >>= combo,
-                1 => b ^= literal as u32,
-                2 => b = combo % 8,
-                3 => {
-                    if a != 0 {
-                        pointer = (literal >> 1) as usize;
-                        continue;
-                    }
-                }
-                4 => b ^= c,
-                5 => return Some((combo % 8) as u8),
-                6 => b = a >> combo,
-                7 => c = a >> combo,
-                _ => unreachable!(),
-            };
-        }
-        None
-    });
+    (executor, program)
+}
 
-    Some(executor.map(|n| n.to_string()).join(","))
+pub fn part_one(input: &str) -> Option<String> {
+    let (mut executor, program) = common(input);
+    Some(executor(program, None).map(|n| n.to_string()).join(","))
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
