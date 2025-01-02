7ecfc54177d326a6d840236f55e94d49fb54ba0a
kumatheworld <kum4th3w0rld@gmail.com>
2025-01-02 21:49:48 +0000

    Day 18 part 1 done

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,6 +1,52 @@
+use advent_of_code::Matrix;
+use itertools::Itertools;
+
 advent_of_code::solution!(18);
 
 pub fn part_one(input: &str) -> Option<u32> {
+    const BOUNDARY: usize = 100;
+    const DIJ: [(i32, i32); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+
+    let coordinates = input
+        .lines()
+        .map(|line| {
+            let (x, y) = line.split_once(",").unwrap();
+            (x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap())
+        })
+        .collect_vec();
+
+    let (size, bytes) = if coordinates.len() < BOUNDARY {
+        (7, 12)
+    } else {
+        (71, 1024)
+    };
+
+    let mut seen = Matrix::uniform(size, size, false);
+    for &(x, y) in &coordinates[..bytes] {
+        seen[(y, x)] = true;
+    }
+    print!("{}", seen);
+
+    let start = (0, 0);
+    let end = (size as i32 - 1, size as i32 - 1);
+    let mut queue = vec![start];
+    seen[start] = true;
+    for k in 1.. {
+        let mut q = vec![];
+        for (i, j) in queue {
+            for (di, dj) in DIJ {
+                let iijj = (i + di, j + dj);
+                if iijj == end {
+                    return Some(k);
+                }
+                if seen.get(iijj) == Some(false) {
+                    seen[iijj] = true;
+                    q.push(iijj);
+                }
+            }
+        }
+        queue = q;
+    }
     None
 }
 
