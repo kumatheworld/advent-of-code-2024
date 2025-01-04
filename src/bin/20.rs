570242b04eb2028a4c7541e8f247f2cd5a6ab872
kumatheworld <kum4th3w0rld@gmail.com>
2025-01-04 18:52:21 +0000

    Day 20 part 1 done

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -1,7 +1,41 @@
+use advent_of_code::{Matrix, DIJ};
+use itertools::Itertools;
+
 advent_of_code::solution!(20);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    const SAVE: u32 = 100;
+
+    let mut mat = Matrix::from(input);
+    let mut dist = mat.new_uniform(None);
+    let start = mat.position(b'S').unwrap();
+    let (mut i, mut j) = start;
+    for k in 0.. {
+        mat[(i, j)] = b'#';
+        dist[(i, j)] = Some(k);
+        let ijs = DIJ
+            .iter()
+            .filter_map(|&(di, dj)| (mat[(i + di, j + dj)] != b'#').then_some((i + di, j + dj)))
+            .collect_vec();
+        match ijs.len() {
+            0 => break,
+            1 => (i, j) = ijs[0],
+            _ => unreachable!(),
+        };
+    }
+
+    Some(
+        dist.inner_indices()
+            .filter(|(i, j)| {
+                let ds = DIJ
+                    .iter()
+                    .filter_map(|(di, dj)| dist[(i + di, j + dj)])
+                    .sorted()
+                    .collect_vec();
+                ds.len() >= 2 && ds[ds.len() - 1] >= ds[0] + SAVE + 2
+            })
+            .count() as u32,
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
