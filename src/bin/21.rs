3b07ac14627851df982a51724c3d1b9a9bb4b5f5
kumatheworld <kum4th3w0rld@gmail.com>
2025-01-17 19:01:51 +0000

    Day 21 part 2 conceivably done

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -66,8 +66,8 @@ pub fn part_one(input: &str) -> Option<u64> {
     common(input, 2)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    common(input, 25)
 }
 
 #[cfg(test)]
