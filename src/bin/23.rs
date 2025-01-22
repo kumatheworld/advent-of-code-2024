cfee916f83785de499e8634bcfb7c50920fcbfbb
kumatheworld <kum4th3w0rld@gmail.com>
2025-01-22 18:54:16 +0000

    Day 23 part 1 done

--- src/bin/23.rs
+++ src/bin/23.rs
@@ -1,7 +1,31 @@
+use std::collections::{HashMap, HashSet};
+use std::convert::TryFrom;
+
+use itertools::Itertools;
+
 advent_of_code::solution!(23);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut graph = HashMap::<[u8; 2], HashSet<[u8; 2]>>::new();
+    for line in input.lines() {
+        let (u, v) = line.split_once('-').unwrap();
+        let u = <[u8; 2]>::try_from(u.as_bytes()).unwrap();
+        let v = <[u8; 2]>::try_from(v.as_bytes()).unwrap();
+        graph.entry(u).or_default().insert(v);
+        graph.entry(v).or_default().insert(u);
+    }
+
+    Some(
+        graph
+            .iter()
+            .flat_map(|(u, vs)| {
+                vs.iter().tuple_combinations().filter(|&(v, w)| {
+                    [u[0], v[0], w[0]].contains(&b't') && graph.get(v).unwrap().contains(w)
+                })
+            })
+            .count() as u32
+            / 3,
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
