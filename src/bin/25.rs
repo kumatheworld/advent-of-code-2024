217c0ae0a4e7b0867f4fe00785252da448bd8041
kumatheworld <kum4th3w0rld@gmail.com>
2025-01-29 21:33:10 +0000

    Day 25 part 1 done

--- src/bin/25.rs
+++ src/bin/25.rs
@@ -1,7 +1,34 @@
+use advent_of_code::Matrix;
+use itertools::{iproduct, Itertools};
+
 advent_of_code::solution!(25);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut locks = Vec::<Vec<i32>>::new();
+    let mut keys = Vec::<Vec<i32>>::new();
+    for block in input.trim().split("\n\n") {
+        let mat = Matrix::from(block);
+        let mat00 = mat[(0, 0)];
+        match mat00 {
+            b'#' => &mut locks,
+            b'.' => &mut keys,
+            _ => unreachable!(),
+        }
+        .push(
+            (0..mat.cols as i32)
+                .map(move |j| {
+                    (1..mat.rows as i32)
+                        .find(|&i| mat[(i, j)] != mat[(0, 0)])
+                        .unwrap()
+                })
+                .collect_vec(),
+        );
+    }
+    Some(
+        iproduct!(locks, keys)
+            .filter(|(l, k)| l.iter().zip_eq(k).all(|(a, b)| a <= b))
+            .count() as u32,
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
